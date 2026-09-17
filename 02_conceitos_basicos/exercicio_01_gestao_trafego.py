7eb3ebb23ed96c5407c51fe7c1a8d89a5c7c537d
dafnemariajesussilva-del <dafnemariajesussilva@gmail.com>
2026-09-17 14:25:46 -0300

    mensagem

--- 02_conceitos_basicos/exercicio_01_gestao_trafego.py
+++ 02_conceitos_basicos/exercicio_01_gestao_trafego.py
@@ -12,3 +12,6 @@ Calcule e mostre na tela o Custo Por Clique (CPC) médio da campanha formatado e
 """
 
 # TODO: Desenvolva o algoritmo abaixo:
+valor_total_investido = float(input("valor investido:"))
+numero_cliques_obtidos = float(input("numero de cliques:"))
+print(f"esse e cpc medio da campanha em reais")
--- 02_conceitos_basicos/pratica_aluno_mao_massa_02.py
+++ 02_conceitos_basicos/pratica_aluno_mao_massa_02.py
@@ -4,3 +4,8 @@
 # 3. Calcule o valor por pessoa
 # 4. Imprima formatado usando f-string
 
+valor_da_conta = float(input("digite o valor da conta: "))
+leia__numero_de_pessoas = float(input("digite o numero de pessoas: "))
+calcule_valor_por_pessoa = valor_da_conta / leia__numero_de_pessoas
+print(f"esse e o valor pago por pessoa {calcule_valor_por_pessoa:.2f}")
+
