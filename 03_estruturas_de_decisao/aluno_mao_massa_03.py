b94b692b70c83e015e4cd376f21221985d296d33
dafnemariajesussilva-del <dafnemariajesussilva@gmail.com>
2026-09-21 14:08:44 -0300

    mensagem

---
+++ 03_estruturas_de_decisao/aluno_mao_massa_03.py
@@ -0,0 +1,6 @@
+#TODO: Implemente a expressão de validação
+media_aluno = 7.5
+frequencia_percentual = 80
+
+aprovado = media_aluno >=6.0 and frequencia_percentual >=75
+print("Status de aprovação:", aprovado)
